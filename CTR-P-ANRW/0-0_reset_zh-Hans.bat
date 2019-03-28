2cd2b78883abb363a159c2fe0946c46e43567206
Sun Daowen <sundaowen@gmail.com>
2019-03-28 20:08:50 +0800

    init CTR-P-ANRW

---
+++ CTR-P-ANRW/0-0_reset_zh-Hans.bat
@@ -0,0 +1,8 @@
+PUSHD "%~dp0"
+RD /S /Q cci
+RD /S /Q build
+DEL "Star Fox 64 3D (TWN) (zh-Hans).3ds"
+DEL patch.3ps
+DEL zh\icon.ico
+DEL zh-Hans_ANRWTOOLS.exe
+POPD
---
+++ CTR-P-ANRW/0-1_reset_zh-Hant.bat
@@ -0,0 +1,8 @@
+PUSHD "%~dp0"
+RD /S /Q cci
+RD /S /Q build
+DEL "Star Fox 64 3D (TWN) (zh-Hant).3ds"
+DEL patch.3ps
+DEL zh\icon.ico
+DEL zh-Hant_ANRWTOOLS.exe
+POPD
